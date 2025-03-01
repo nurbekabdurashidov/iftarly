816995482404780ebb8238ca066437aa1096ad66
djangobek <nnjjmmjj8@gmail.com>
2025-03-01 16:32:36 +0500

    AA

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -13,7 +13,7 @@ import io
 from aiogram.types import BufferedInputFile, InlineKeyboardMarkup
 import aiohttp
 
-API_URL = "https://iftarly.pythonanywhere.com/"
+API_URL = "http://127.0.0.1:8000"
 
 # Tillar
 LANGUAGES = {
